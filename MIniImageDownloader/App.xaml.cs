055b7bddbeb449c4d20a1dbf4ce5c81f7e48c4f0
nikita.moiseev <moiseevnikitaa@gmail.com>
2016-04-20 07:07:14 +0300

    + begin global hotkey

--- MIniImageDownloader/App.xaml.cs
+++ MIniImageDownloader/App.xaml.cs
@@ -11,6 +11,7 @@ namespace MIniImageDownloader
     /// </summary>
     public partial class App : Application
     {
+        HotkeyHelper _hotkeyHelper;
         TaskbarIcon _taskBarIcon;
 
         protected override void OnStartup(StartupEventArgs e)
@@ -21,22 +22,13 @@ namespace MIniImageDownloader
 
             _taskBarIcon = (TaskbarIcon)FindResource("TaskBarIcon");
             _taskBarIcon.DataContext = locator.TaskBar;
-        }
 
-        protected override void OnFragmentNavigation(FragmentNavigationEventArgs e)
-        {
-            base.OnFragmentNavigation(e);
-        }
-
-        protected override void OnNavigated(NavigationEventArgs e)
-        {
-            base.OnNavigated(e);
-
-            //Current.MainWindow.Hide();
+            _hotkeyHelper.Register(Current.MainWindow = new MainWindow());
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
+            _hotkeyHelper.Unregister();
             base.OnExit(e);
         }
     }
